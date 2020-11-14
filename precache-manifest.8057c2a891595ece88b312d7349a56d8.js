afa632858acbbfcea33cd64355b091dbf271711b
MuhammadFaizan1122 <59656222+MuhammadFaizan1122@users.noreply.github.com>
2020-11-14 20:12:31 +0500

    Olx Replica

---
+++ precache-manifest.8057c2a891595ece88b312d7349a56d8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4234ce250a3e683c3338f7a35a67e36a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7e1ebd012b4de19d296",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "59a74f9d5655051daae3",
+    "url": "/static/css/main.44973a64.chunk.css"
+  },
+  {
+    "revision": "c7e1ebd012b4de19d296",
+    "url": "/static/js/2.5097428d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5097428d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a74f9d5655051daae3",
+    "url": "/static/js/main.acfb25f1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "f23ad0c1d7a31d5ef61301da3a2bd152",
+    "url": "/static/media/Olx-Logo.f23ad0c1.png"
+  },
+  {
+    "revision": "70670fba1bcc3edaa270906dbd134e6f",
+    "url": "/static/media/car.70670fba.jpg"
+  }
+]);
